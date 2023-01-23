b13ca8c92e696906ca0a34b51ca6e21507434c2d
Jihana <wldnjs7618@naver.com>
2023-01-24 03:02:13 +0900

    youtube title

--- Youtube/basic/src/App.js
+++ Youtube/basic/src/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Outlet } from 'react-router-dom';
+import SearchHeader from './components/SearchHeader';
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <></>
+    <>
+      <SearchHeader/>
+      <QueryClientProvider client={queryClient}>
+        <Outlet/>
+      </QueryClientProvider>
+    </>
   );
 }
 
